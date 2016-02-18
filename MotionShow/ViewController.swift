b413d29fefcb190c8bde268ca95c2cd88073bd3d
vinigodoy <vinigodoy@gmail.com>
2016-02-18 01:31:37 -0200

    Initial Commit

---
+++ MotionShow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MotionShow
+//
+//  Created by vinigodoy on 18/02/16.
+//  Copyright © 2016 pucpr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
