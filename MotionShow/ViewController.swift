af20b8741eacb5f3968b9936426d0a60d84ac1cc
Rodrigo Fragozo <rsfragozo@Rodrigos-iMac.local>
2017-05-29 20:12:37 +0200

    Initial commit

--- MotionShow/ViewController.swift
+++ MotionShow/ViewController.swift
@@ -7,19 +7,112 @@
 //
 
 import UIKit
+import CoreMotion
 
-class ViewController: UIViewController {
+class ViewController: UIViewController{
+    @IBOutlet var lblAccX: UILabel!
+    @IBOutlet var lblAccY: UILabel!
+    @IBOutlet var lblAccZ: UILabel!
+    @IBOutlet var lblMaxAccX: UILabel!
+    @IBOutlet var lblMaxAccY: UILabel!
+    @IBOutlet var lblMaxAccZ: UILabel!
+    
+    @IBOutlet var lblRotX: UILabel!
+    @IBOutlet var lblRotY: UILabel!
+    @IBOutlet var lblRotZ: UILabel!
+    @IBOutlet var lblMaxRotX: UILabel!
+    @IBOutlet var lblMaxRotY: UILabel!
+    @IBOutlet var lblMaxRotZ: UILabel!
 
+    var currentMaxAccelX: Double = 0.0
+    var currentMaxAccelY: Double = 0.0
+    var currentMaxAccelZ: Double = 0.0
+    
+    var currentMaxRotX: Double = 0.0
+    var currentMaxRotY: Double = 0.0
+    var currentMaxRotZ: Double = 0.0
+    
+    var movementManager = CMMotionManager()
+    
+    
+    @IBAction func resetMaxValues(_ sender: AnyObject) {
+        currentMaxAccelX = 0
+        currentMaxAccelY = 0
+        currentMaxAccelZ = 0
+        
+        currentMaxRotX = 0
+        currentMaxRotY = 0
+        currentMaxRotZ = 0
+    }
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        currentMaxAccelX = 0
+        currentMaxAccelY = 0
+        currentMaxAccelZ = 0
+        
+        currentMaxRotX = 0
+        currentMaxRotY = 0
+        currentMaxRotZ = 0
+        
+        movementManager.gyroUpdateInterval = 0.2
+        movementManager.accelerometerUpdateInterval = 0.2
+        
+        //Inicia o rastreamento do movimento
+        movementManager.startAccelerometerUpdates(to: OperationQueue.current!) { (accelerometerData: CMAccelerometerData?, NSError) -> Void in
+            
+            self.outputAccData(accelerometerData!.acceleration)
+            if(NSError != nil) {
+                print("\(NSError)")
+            }
+        }
+        
+        movementManager.startGyroUpdates(to: OperationQueue.current!, withHandler: { (gyroData: CMGyroData?, NSError) -> Void in
+            self.outputRotData(gyroData!.rotationRate)
+            if (NSError != nil){
+                print("\(NSError)")
+            }
+        })
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func outputAccData(_ acceleration: CMAcceleration){
+        lblAccX?.text = "\(acceleration.x).2fg"
+        if fabs(acceleration.x) > fabs(currentMaxAccelX) {
+            currentMaxAccelX = acceleration.x
+        }
+        
+        lblAccY?.text = "\(acceleration.y).2fg"
+        if fabs(acceleration.y) > fabs(currentMaxAccelY) {
+            currentMaxAccelY = acceleration.y
+        }
+        
+        lblAccZ?.text = "\(acceleration.z).2fg"
+        if fabs(acceleration.z) > fabs(currentMaxAccelZ) {
+            currentMaxAccelZ = acceleration.z
+        }
+        
+        lblMaxAccX?.text = "\(currentMaxAccelX).2f"
+        lblMaxAccY?.text = "\(currentMaxAccelY).2f"
+        lblMaxAccZ?.text = "\(currentMaxAccelZ).2f"
     }
+    
+    func outputRotData(_ rotation: CMRotationRate) {
+        lblRotX?.text = "\(rotation.x).2fr/s"
+        if fabs(rotation.x) > fabs(currentMaxRotX) {
+            currentMaxRotX = rotation.x
+        }
+        
+        lblRotY?.text = "\(rotation.y).2fr/s"
+        if fabs(rotation.y) > fabs(currentMaxRotY) {
+            currentMaxRotY = rotation.y
+        }
+        
+        lblRotZ?.text = "\(rotation.z).2fr/s"
+        if fabs(rotation.z) > fabs(currentMaxRotZ) {
+            currentMaxRotZ = rotation.z
+        }
 
-
+        lblMaxRotX?.text = "\(currentMaxRotX).2f"
+        lblMaxRotY?.text = "\(currentMaxRotY).2f"
+        lblMaxRotZ?.text = "\(currentMaxRotZ).2f"
+    }
 }
-
